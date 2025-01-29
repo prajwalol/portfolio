27e5c87e6b61422a7a63e640015e0247c1a9ca11
prajwalol <shresthaprajwal076@gmail.com>
2025-01-29 16:12:23 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import 'font-awesome/css/font-awesome.min.css';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div>
-     
       <About />
       <Skills />
       <Projects />
